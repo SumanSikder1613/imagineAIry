2c95d0326285c779aa491f82a8f4caafc9b80ca8
SUMAN SIKDER <sikdersumn@gmail.com>
2023-12-12 18:29:37 +0530

    vertical & horizontal nav

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const menuBtn = document.querySelector('.menu-btn');
+const sideNavbar=document.querySelector('.sideNavbar');
 
-
-
+menuBtn.addEventListener("click",()=>{
+sideNavbar.classList.toggle("active");
+});
